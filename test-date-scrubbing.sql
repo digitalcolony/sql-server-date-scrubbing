1447489533ef484ad33adca7d8f9816d63b2450f
MAS <digitalcolony@gmail.com>
2017-10-11 08:04:16 -0700

    made prety for GitHub

--- test-date-scrubbing.sql
+++ test-date-scrubbing.sql
@@ -1,8 +1,3 @@
-# SQL Server - Date Scrubbing UDF
-
-Here is a handy little UDF that rounds a DATETIME column to either the MINUTE, HOUR, DAY, WEEK or MONTH.
-
-```sql
 DECLARE @now AS DATETIME
 SET @now = getDate()
 
@@ -12,6 +7,3 @@ SELECT dbo.udfDateFirst('DAY',@now)
 SELECT dbo.udfDateFirst('WEEK',@now)
 SELECT dbo.udfDateFirst('MONTH',@now)
 SELECT dbo.udfDateFirst('YEAR',@now)
-```
-
-_created: 2002 or 2003_
